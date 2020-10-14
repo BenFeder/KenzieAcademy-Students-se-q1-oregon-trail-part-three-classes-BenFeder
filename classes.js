c92941b6ad6dcdd8df181bc836af5c32aa3f517e
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-10-14 13:42:24 +0000

    deploy: 33264e334cf77d45a4ad665d80f3a0f3e0c035eb

--- classes.js
+++ classes.js
@@ -2,7 +2,95 @@
  * Oregon Trail -----------------------------------------------------------
  */
 
-// Create your Classes here.
+class Traveler {
+  constructor(name) {
+    this.name = name;
+    this.food = 1;
+    this.isHealthy = true;
+  }
+
+  hunt() {
+    this.food += 2;
+  }
+
+  eat() {
+    if (this.food == 0) {
+      this.isHealthy = false;
+    } else {
+      this.food--;
+    }
+  }
+}
+
+class Doctor extends Traveler {
+  constructor(name) {
+    super(name);
+  }
+  heal(traveler) {
+    traveler.isHealthy = true;
+  }
+}
+
+class Hunter extends Traveler {
+  constructor(name) {
+    super(name);
+    this.food = 2;
+  }
+  hunt() {
+    this.food += 5;
+  }
+  eat() {
+    if (this.food >= 2) {
+      this.food -= 2;
+    } else if (this.food == 1) {
+      this.food--;
+      this.isHealthy = false;
+    } else if (this.food == 0) {
+      this.isHealthy = false;
+    }
+  }
+
+  giveFood(traveler, numOfFoodUnits) {
+    if (numOfFoodUnits <= this.food) {
+      this.food -= numOfFoodUnits;
+      traveler.food += numOfFoodUnits;
+    }
+  }
+}
+
+class Wagon {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.passengers = [];
+  }
+
+  getAvailableSeatCount() {
+    return this.capacity - this.passengers.length;
+  }
+
+  join(traveler) {
+    if (this.getAvailableSeatCount() > 0) {
+      this.passengers.push(traveler);
+    }
+  }
+
+  shouldQuarantine() {
+    for (let traveler = 0; traveler < this.passengers.length; traveler++) {
+      if (this.passengers[traveler].isHealthy == false) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  totalFood() {
+    let totalFood = 0;
+    for (let traveler = 0; traveler < this.passengers.length; traveler++) {
+      totalFood += this.passengers[traveler].food;
+    }
+    return totalFood;
+  }
+}
 
 /**
  * TESTS -----------------------------------------------------------
